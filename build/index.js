c4da6e11fa235af814bb930c70d5d38767af3be5
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-21 23:12:14 -0700

    build index.js

--- build/index.js
+++ build/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('testing 1, 2');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+//add .csv file into code and then console log entries
+var entries = fs_1.default.readFileSync('.csv', {
+    encoding: 'utf-8',
+});

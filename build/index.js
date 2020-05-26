6922fa58f18dc3d2e931e28b2022efd4c1d9fbf3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2020-05-25 21:45:05 -0700

    edited characters in csv file

--- build/index.js
+++ build/index.js
@@ -5,6 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = __importDefault(require("fs"));
 //add .csv file into code and then console log entries
-var entries = fs_1.default.readFileSync('.csv', {
+var entries = fs_1.default.readFileSync('eurovision.csv', {
     encoding: 'utf-8',
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
 //add .csv file into code and then console log entries
-const entries = fs.readFileSync('.csv', {
+const entries = fs.readFileSync('eurovision.csv', {
   encoding: 'utf-8',
 });
